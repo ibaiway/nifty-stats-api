6d7ebb63a73fc449c477e38cec905fcf3cffcff8
jseo123 <jose123135@gmail.com>
2022-05-09 15:25:34 +0200

    Added response to controller functions

--- src/app/Http/Controllers/PlaybackController.php
+++ src/app/Http/Controllers/PlaybackController.php
@@ -8,84 +8,15 @@ use Illuminate\Support\Facades\DB;
 
 class PlaybackController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
 
     public function countPlaybacks(Request $request)
     {
     //   $tracksIds =  Playback::all()->count()->pluck('trackId')->toArray();
     $tracksIds = Playback::select('trackId', DB::raw('count(*) as total'))->groupBy('trackId')->get();
-    return $tracksIds;
+    return response()->json([
+        'data' => $tracksIds,
+    ], status:201);
     }
 
     public function store(Request $request)
@@ -95,6 +26,9 @@ class PlaybackController extends Controller
         $playback->userId = $request->userId;
         $playback->agent = $request->agent;
         $playback->save();
+        return response()->json([
+            'message' => 'playback recorded'
+        ], status:201);
     }
 
 }
